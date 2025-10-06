7e24bf2867ebf018fbf3c6c058d5db2e4201e4d4
Amirjon Ulmasov <amir@Amirjons-MacBook-Air.local>
2025-10-06 19:42:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+![screenshot](images/bloodlines_screenshot.png)
+
+## Bloodlines
+
+The assignment description can be found [here](https://docs.google.com/document/d/1r315RI4HrGn_lHxQ0lV1yRx49AyMThAPbZtxbQR0qKo/edit?usp=sharing).
+
+This repository contains starter code:
+- `factor.py`: Code for questions one, two, and three goes in this file
+- `bayes.py`: Code for question four and five goes in this file
+- `genetics.py`: Code for questions six, seven, eight, nine, and ten goes in this file
+
+It also contains code that you can use and/or emulate but should not change:
+- `montyhall.py`: The Monty Hall Bayesian network discussed in class.
+- `vampire.py`: The blood types Bayesian network discussed in class.
+- `util.py`: Contains the utility function `compute_elimination_order`. You do not have to understand how this function works.
+
+Finally it contains the visualizer:
+- `bloodlines.py`: Run this from the command-line once you've completed questions one through ten.
+
+Ignore (but do not delete!!!) the other files in this repository.
---
+++ factor.py
@@ -0,0 +1,39 @@
+import itertools
+from itertools import product
+
+class Factor:
+    def __init__(self, variables, values):
+        self.variables = variables
+        self.values = values
+
+    def __getitem__(self, event):
+        key = []
+        for var in self.variables:
+            if var not in event:
+                raise KeyError(f"Variable {var} not found in given event.")
+            key.append(event[var])
+        if tuple(key) in self.values:
+            return self.values[tuple(key)]
+        else:
+            raise KeyError(f"No value assigned to event {event}.")
+
+    def __str__(self):
+        result = f"{self.variables}:"
+        for event, value in self.values.items():
+            result += f"\n  {event}: {value}"
+        return result
+
+    __repr__ = __str__
+
+
+def events(vars, domains):
+    thing = list(itertools.product(list(zip(vars,element)) for element in domains))
+ 
+    return thing
+    
+
+def marginalize(factor, variable):
+    """TODO: Implement this for Question Two."""
+
+def multiply_factors(factors, domains):
+    """TODO: Implement this for Question Three."""
